098571d4ec2ad301fb9684dc81a8f923524d1654
Akshit8459 <akshitpokhriyal@gmail.com>
2025-06-30 13:08:40 +0530

    basic crud using mern

--- 2.DBApps/BasicMongoServer/app.js
+++ 2.DBApps/BasicMongoServer/app.js
@@ -38,6 +38,24 @@ app.get("/about",async (req,res)=>{
         </ul></body></html>`);
 })
 
+//to get single user
+app.get("/about/:id", async (req,res)=>{
+    res.setHeader('Surrogate-Control', 'no-store');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Expires', '0');
+    const id=req.params.id;
+    const user=await Users.findOne({id:id});
+    res.send(`<html>
+        <head>
+        <meta http-equiv="refresh" content="5">
+        <title>MONGO SERVER</title>
+        </head>
+        <body>
+        <h1> ${user.name}:</h1>
+        <li>${user.age}</li>
+        </ul></body></html>`)
+})
+
 //to add users
 app.post("/about",async (req,res)=>{
     const user=req.body;
@@ -59,9 +77,23 @@ app.post("/about",async (req,res)=>{
 })
 
 //to update a user
-
+app.put("/about/:id", async (req,res)=>{
+    res.setHeader('Surrogate-Control', 'no-store');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Expires', '0');
+    const id=req.params.id;
+    let user=await Users.findOne({id:id});
+    const name=req.body.name || user.name;
+    const age=req.body.age || user.age;
+    user=await Users.findOneAndUpdate({id:id},{name:name,age:age,id:id}, {new:true});
+    res.send(`User updated successfully:${user}`);
+})
 
 // to delete a user
+app.delete("/about/:id", async (req,res)=>{
+    const user=await Users.findOneAndDelete({id:req.params.id});
+    res.send(`User Deleted Successfully: ${user}`);
+})
 
 app.listen(process.env.PORT,()=>{
     console.log(`server running on port ${process.env.PORT}`);
